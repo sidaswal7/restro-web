6c3bb35ce7e1597aefb8dfe147ce7515b69692ab
sidaswal7 <siddhantaswal7@gmail.com>
2024-05-04 10:28:51 +0530

    added mealitems

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,45 @@
+import React from "react"
+import Card from "../UI/Card";
+import MealItems from "./MealItems"
+import classes from "./AvailableMeals.module.css"
+const DUMMY_MEALS = [
+    {
+      id: 'm1',
+      name: 'Sushi',
+      description: 'Finest fish and veggies',
+      price: 22.99,
+    },
+    {
+      id: 'm2',
+      name: 'Schnitzel',
+      description: 'A german specialty!',
+      price: 16.5,
+    },
+    {
+      id: 'm3',
+      name: 'Barbecue Burger',
+      description: 'American, raw, meaty',
+      price: 12.99,
+    },
+    {
+      id: 'm4',
+      name: 'Green Bowl',
+      description: 'Healthy...and green...',
+      price: 18.99,
+    },
+  ];
+
+const AvailableMeals = ()=>{
+    const mealList = DUMMY_MEALS.map(meal=><MealItems key={meal.id} meal={meal}/>)
+    return( 
+    <div className={classes.meals}>
+        <Card>
+            <ul>
+                {mealList}
+            </ul>
+        </Card>
+        
+    </div>
+)}
+
+export default AvailableMeals;
--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,11 +1,13 @@
 import React from "react";
 import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
 
 const Meals = ()=>{
 
     return(
         <>
             <MealsSummary/>
+            <AvailableMeals/>
         </>
     )
 }
